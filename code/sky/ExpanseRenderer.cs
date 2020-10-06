c92bd11a1767973e2bd06b5ab0edb4b1ac1b6c16
Brad Guesman <james_guesman@alumni.brown.edu>
2020-10-05 20:11:49 -0700

    Got rendertextures working for compositing.

--- code/sky/ExpanseRenderer.cs
+++ code/sky/ExpanseRenderer.cs
@@ -35,8 +35,7 @@ private RTHandle[] m_GITables;                    /* Ground Irradiance. */
 private Expanse.ExpanseCommon.SkyTextureQuality m_skyTextureQuality;
 private int m_numAtmosphereLayersEnabled = 0;
 /* Allocates all sky precomputation tables for all atmosphere layers at a
- * specified quality level.
- * TODO: something seems to work probabilistically about this function. */
+ * specified quality level. */
 void allocateSkyPrecomputationTables(ExpanseSky sky) {
 
   /* Count how many layers are active. */
@@ -164,6 +163,107 @@ RTHandle allocateSky4DTable(Vector4 resolution, int index, string name) {
 
 
 /******************************************************************************/
+/******************************** FRAMEBUFFERS ********************************/
+/******************************************************************************/
+
+/* 2 tables:
+ *  1. Sky color. RGBA where A isn't used (currently).
+ *  2. Sky transmittance. RGBA where A isn't used (currently). Computed
+ *    using depth buffer. */
+struct SkyRenderTexture {
+  public RTHandle colorBuffer;
+  public RTHandle transmittanceBuffer;
+};
+SkyRenderTexture m_fullscreenSkyRT;
+SkyRenderTexture m_cubemapSkyRT;
+
+/* 2 sets of 2 tables:
+ *  1. Clouds lighting. RGBA where A isn't used (currently).
+ *  2. Clouds transmittance. RGBA where RGB is transmittance and A is
+ *    atmospheric blend factor.
+ * There are 2 sets of tables because we need to maintain a "previous"
+ * table for reprojection. We alternate between which is the previous
+ * and which is the current render texture set to avoid  */
+struct CloudRenderTexture {
+ public RTHandle colorBuffer;
+ public RTHandle transmittanceBuffer;
+};
+CloudRenderTexture[] m_fullscreenCloudRT;
+CloudRenderTexture[] m_cubemapCloudRT;
+int m_currentFullscreenCloudsRT;
+int m_currentCubemapCloudsRT;
+Vector2 m_currentFullscreenRTSize;
+Vector2 m_currentCubemapRTSize;
+
+SkyRenderTexture buildSkyRenderTexture(Vector2 resolution, int index, string name) {
+  SkyRenderTexture r = new SkyRenderTexture();
+  r.colorBuffer = allocateSky2DTable(resolution, index, name + "_Color");
+  r.transmittanceBuffer = allocateSky2DTable(resolution, index, name + "_Transmittance");
+  return r;
+}
+
+CloudRenderTexture buildCloudRenderTexture(Vector2 resolution, int index, string name) {
+  CloudRenderTexture r = new CloudRenderTexture();
+  r.colorBuffer = allocateSky2DTable(resolution, index, name + "_Color");
+  r.transmittanceBuffer = allocateSky2DTable(resolution, index, name + "_Transmittance");
+  return r;
+}
+
+private void buildFullscreenRenderTextures(Vector2 resolution) {
+  m_fullscreenSkyRT = buildSkyRenderTexture(resolution, 0, "fullscreenSkyRT");
+  m_fullscreenCloudRT = new CloudRenderTexture[2];
+  for (int i = 0; i < 2; i++) {
+    m_fullscreenCloudRT[i] = buildCloudRenderTexture(resolution, i, "fullscreenCloudRT");
+  }
+  m_currentFullscreenCloudsRT = 0;
+  m_currentFullscreenRTSize = resolution;
+}
+
+private void buildCubemapRenderTextures(Vector2 resolution) {
+  m_cubemapSkyRT = buildSkyRenderTexture(resolution, 0, "cubemapSkyRT");
+  m_cubemapCloudRT = new CloudRenderTexture[2];
+  for (int i = 0; i < 2; i++) {
+    m_cubemapCloudRT[i] = buildCloudRenderTexture(resolution, i, "cubemapCloudRT");
+  }
+  m_currentCubemapCloudsRT = 0;
+  m_currentCubemapRTSize = resolution;
+}
+
+private void cleanupFullscreenRenderTextures() {
+  RTHandles.Release(m_fullscreenSkyRT.colorBuffer);
+  RTHandles.Release(m_fullscreenSkyRT.transmittanceBuffer);
+  m_fullscreenSkyRT.colorBuffer = null;
+  m_fullscreenSkyRT.transmittanceBuffer = null;
+
+  for (int i = 0; i < 2; i++) {
+    RTHandles.Release(m_fullscreenCloudRT[i].colorBuffer);
+    RTHandles.Release(m_fullscreenCloudRT[i].transmittanceBuffer);
+    m_fullscreenCloudRT[i].colorBuffer = null;
+    m_fullscreenCloudRT[i].transmittanceBuffer = null;
+  }
+}
+
+private void cleanupCubemapRenderTextures() {
+  RTHandles.Release(m_cubemapSkyRT.colorBuffer);
+  RTHandles.Release(m_cubemapSkyRT.transmittanceBuffer);
+  m_cubemapSkyRT.colorBuffer = null;
+  m_cubemapSkyRT.transmittanceBuffer = null;
+
+  for (int i = 0; i < 2; i++) {
+    RTHandles.Release(m_cubemapCloudRT[i].colorBuffer);
+    RTHandles.Release(m_cubemapCloudRT[i].transmittanceBuffer);
+    m_cubemapCloudRT[i].colorBuffer = null;
+    m_cubemapCloudRT[i].transmittanceBuffer = null;
+  }
+}
+
+/******************************************************************************/
+/****************************** END FRAMEBUFFERS ******************************/
+/******************************************************************************/
+
+
+
+/******************************************************************************/
 /****************************** MEMBER VARIABLES ******************************/
 /******************************************************************************/
 
@@ -176,21 +276,23 @@ private static int m_RenderCubemapSkyID = 0;       // FragBakeSky
 private static int m_RenderFullscreenSkyID = 1;    // FragRenderSky
 private static int m_RenderCubemapCloudsID = 2;    // FragBakeClouds
 private static int m_RenderFullscreenCloudsID = 3; // FragRenderClouds
-private static int m_CompositeCubemapSkyAndCloudsID = 2;    // FragBakeComposite // TODO: wrong temporarily
-private static int m_CompositeFullscreenSkyAndCloudsID = 3; // FragRenderComposite
+private static int m_CompositeCubemapSkyAndCloudsID = 4;    // FragBakeComposite // TODO: wrong temporarily
+private static int m_CompositeFullscreenSkyAndCloudsID = 5; // FragRenderComposite
 
 /******************************************************************************/
 /**************************** END MEMBER VARIABLES ****************************/
 /******************************************************************************/
 
-RTHandle[] m_skyRT;
 
 public override void Build() {
   /* Create material for sky shader. */
   m_skyMaterial = CoreUtils.CreateEngineMaterial(GetSkyShader());
 
-  m_skyRT = new RTHandle[1];
-  m_skyRT[0] = allocateSky2DTable(new Vector2(Screen.width, Screen.height), 0, "ScreenRT");
+  /* Create the framebuffers we'll use for our multi-pass strategy.
+   * This width and height is a best guess. It will be reset in the render
+   * function if it's wrong. */
+  buildFullscreenRenderTextures(new Vector2(Screen.width, Screen.height));
+  buildCubemapRenderTextures(new Vector2(Screen.width, Screen.height));
 }
 
 /* Returns reference to Expanse sky shader. */
@@ -201,11 +303,13 @@ Shader GetSkyShader() {
 public override void Cleanup()
 {
   CoreUtils.Destroy(m_skyMaterial);
+
   for (int i = 0; i < m_numAtmosphereLayersEnabled; i++) {
     releaseTablesAtIndex(i);
   }
-  RTHandles.Release(m_skyRT[0]);
-  m_skyRT[0] = null;
+
+  cleanupFullscreenRenderTextures();
+  cleanupCubemapRenderTextures();
 }
 
 protected override bool Update(BuiltinSkyParameters builtinParams)
@@ -239,6 +343,93 @@ protected override bool Update(BuiltinSkyParameters builtinParams)
   return false;
 }
 
+private void RenderSkyPass(BuiltinSkyParameters builtinParams, bool renderForCubemap) {
+  int skyPassID = renderForCubemap ? m_RenderCubemapSkyID : m_RenderFullscreenSkyID;
+  SkyRenderTexture outTex = renderForCubemap ? m_cubemapSkyRT : m_fullscreenSkyRT;
+  RenderTargetIdentifier[] outputs = new RenderTargetIdentifier[] {
+    new RenderTargetIdentifier(outTex.colorBuffer),
+    new RenderTargetIdentifier(outTex.transmittanceBuffer)
+  };
+  if (renderForCubemap) {
+    CoreUtils.DrawFullScreen(builtinParams.commandBuffer, m_skyMaterial,
+      outputs, m_PropertyBlock, skyPassID);
+  } else {
+    CoreUtils.DrawFullScreen(builtinParams.commandBuffer, m_skyMaterial,
+      outputs, builtinParams.depthBuffer, m_PropertyBlock, skyPassID);
+  }
+}
+
+private void RenderCloudsPass(BuiltinSkyParameters builtinParams, bool renderForCubemap) {
+  int skyPassID = renderForCubemap ? m_RenderCubemapCloudsID : m_RenderFullscreenCloudsID;
+  int currentRTID = renderForCubemap ? m_currentCubemapCloudsRT : m_currentFullscreenCloudsRT;
+  int prevRTID = (currentRTID + 1) % 2;
+
+  /* Pass in the previous cloud render texture to use for reprojection.
+   * TODO: probably also pass in camera velocity here. */
+  CloudRenderTexture prevTex = (renderForCubemap ? m_cubemapCloudRT : m_fullscreenCloudRT)[prevRTID];
+  if (renderForCubemap) {
+    m_PropertyBlock.SetTexture("_lastCubemapCloudColorRT", prevTex.colorBuffer);
+    m_PropertyBlock.SetTexture("_lastCubemapCloudTransmittanceRT", prevTex.transmittanceBuffer);
+  } else {
+    m_PropertyBlock.SetTexture("_lastFullscreenCloudColorRT", prevTex.colorBuffer);
+    m_PropertyBlock.SetTexture("_lastFullscreenCloudTransmittanceRT", prevTex.transmittanceBuffer);
+  }
+
+  /* Set the render targets. */
+  CloudRenderTexture outTex = (renderForCubemap ? m_cubemapCloudRT : m_fullscreenCloudRT)[currentRTID];
+  RenderTargetIdentifier[] outputs = new RenderTargetIdentifier[] {
+    new RenderTargetIdentifier(outTex.colorBuffer),
+    new RenderTargetIdentifier(outTex.transmittanceBuffer)
+  };
+
+  /* Draw the clouds and update current render texture. */
+  if (renderForCubemap) {
+    CoreUtils.DrawFullScreen(builtinParams.commandBuffer, m_skyMaterial,
+      outputs, m_PropertyBlock, skyPassID);
+    m_currentCubemapCloudsRT = prevRTID;
+  } else {
+    CoreUtils.DrawFullScreen(builtinParams.commandBuffer, m_skyMaterial,
+      outputs, builtinParams.depthBuffer, m_PropertyBlock, skyPassID);
+    m_currentFullscreenCloudsRT = prevRTID;
+  }
+}
+
+private void RenderCompositePass(BuiltinSkyParameters builtinParams, bool renderForCubemap) {
+  /* Set all the textures we just rendered to to be available in the
+   * shader. */
+  m_PropertyBlock.SetTexture("_fullscreenSkyColorRT", m_fullscreenSkyRT.colorBuffer);
+  m_PropertyBlock.SetTexture("_fullscreenSkyTransmittanceRT", m_fullscreenSkyRT.transmittanceBuffer);
+  m_PropertyBlock.SetTexture("_fullscreenSkyColorRT", m_fullscreenSkyRT.colorBuffer);
+  m_PropertyBlock.SetTexture("_fullscreenSkyTransmittanceRT", m_fullscreenSkyRT.transmittanceBuffer);
+
+  /* Composite. */
+  int compositePassID = renderForCubemap ?
+    m_CompositeCubemapSkyAndCloudsID : m_CompositeFullscreenSkyAndCloudsID;
+
+  if (renderForCubemap) {
+    CoreUtils.DrawFullScreen(builtinParams.commandBuffer, m_skyMaterial,
+      builtinParams.colorBuffer, m_PropertyBlock, compositePassID);
+  } else {
+    CoreUtils.DrawFullScreen(builtinParams.commandBuffer, m_skyMaterial,
+      builtinParams.colorBuffer, builtinParams.depthBuffer, m_PropertyBlock, compositePassID);
+  }
+}
+
+private void checkAndResizeFramebuffers(BuiltinSkyParameters builtinParams, bool renderForCubemap) {
+  Vector2 currSize = (renderForCubemap) ? m_currentCubemapRTSize : m_currentFullscreenRTSize;
+  Vector2Int trueSize = Vector2Int.Max(new Vector2Int(1, 1),
+    builtinParams.colorBuffer.GetScaledSize(builtinParams.colorBuffer.referenceSize));
+  if (currSize.x != trueSize.x || currSize.y != trueSize.y) {
+    if (renderForCubemap) {
+      cleanupCubemapRenderTextures();
+      buildCubemapRenderTextures(trueSize);
+    } else {
+      cleanupFullscreenRenderTextures();
+      buildFullscreenRenderTextures(trueSize);
+    }
+  }
+}
+
 public override void RenderSky(BuiltinSkyParameters builtinParams, bool renderForCubemap, bool renderSunDisk)
 {
   using (new ProfilingSample(builtinParams.commandBuffer, "Draw sky"))
@@ -246,26 +437,29 @@ public override void RenderSky(BuiltinSkyParameters builtinParams, bool renderFo
     /* Get sky object. */
     var sky = builtinParams.skySettings as ExpanseSky;
 
+    /* Check whether or not we have to resize the framebuffers, and do it
+     * if we have to. */
+    checkAndResizeFramebuffers(builtinParams, renderForCubemap);
+
     /* Set material properties for sky and clouds. */
     float intensity = GetSkyIntensity(sky, builtinParams.debugSettings);
     float phi = -Mathf.Deg2Rad * sky.rotation.value; // -rotation to match Legacy
     m_PropertyBlock.SetVector(_SkyParam, new Vector4(intensity, 0.0f, Mathf.Cos(phi), Mathf.Sin(phi)));
     m_PropertyBlock.SetMatrix(_PixelCoordToViewDirWS, builtinParams.pixelCoordToViewDirMatrix);
 
+    /* Set the depth buffer to use when computing transmittance. */
+    if (!renderForCubemap) {
+      m_PropertyBlock.SetTexture("_depthBuffer", builtinParams.depthBuffer);
+    }
+
     /* Render sky pass. */
-
-    int skyPassID = renderForCubemap ? m_RenderCubemapSkyID : m_RenderFullscreenSkyID;
-    CoreUtils.DrawFullScreen(builtinParams.commandBuffer, m_skyMaterial, m_skyRT[0], m_PropertyBlock, skyPassID);
-
-    m_PropertyBlock.SetTexture("_SkyRTFullscreen", m_skyRT[0]);
+    RenderSkyPass(builtinParams, renderForCubemap);
 
     /* Render clouds pass. */
-    /* Composite. */
-    int compositePassID = renderForCubemap ?
-      m_CompositeCubemapSkyAndCloudsID : m_CompositeFullscreenSkyAndCloudsID;
-    if (!renderForCubemap) {
-      CoreUtils.DrawFullScreen(builtinParams.commandBuffer, m_skyMaterial, builtinParams.colorBuffer, builtinParams.depthBuffer, m_PropertyBlock, compositePassID);
-    }
+    RenderCloudsPass(builtinParams, renderForCubemap);
+
+    /* Composite the two together. */
+    RenderCompositePass(builtinParams, renderForCubemap);
   }
 }
 
--- code/sky/ExpanseSky.shader
+++ code/sky/ExpanseSky.shader
@@ -1,168 +1,282 @@
 Shader "Hidden/HDRP/Sky/ExpanseSky"
 {
-  HLSLINCLUDE
 
-  #pragma vertex Vert
+/******************************************************************************/
+/********************************** INCLUDES **********************************/
+/******************************************************************************/
 
-  #pragma editor_sync_compilation
-  #pragma target 4.5
-  #pragma only_renderers d3d11 ps4 xboxone vulkan metal switch
+HLSLINCLUDE
 
-  #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
-  #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/CommonLighting.hlsl"
-  #include "Packages/com.unity.render-pipelines.high-definition/Runtime/Lighting/LightDefinition.cs.hlsl"
-  #include "Packages/com.unity.render-pipelines.high-definition/Runtime/Sky/SkyUtils.hlsl"
-  #include "Packages/com.unity.render-pipelines.high-definition/Runtime/Lighting/LightLoop/CookieSampling.hlsl"
+#pragma vertex Vert
 
-  float4 _SkyParam; // x exposure, y multiplier, zw rotation (cosPhi and sinPhi)
+#pragma editor_sync_compilation
+#pragma target 4.5
+#pragma only_renderers d3d11 ps4 xboxone vulkan metal switch
 
-  #define _Intensity          _SkyParam.x
-  #define _CosPhi             _SkyParam.z
-  #define _SinPhi             _SkyParam.w
-  #define _CosSinPhi          _SkyParam.zw
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/CommonLighting.hlsl"
+#include "Packages/com.unity.render-pipelines.high-definition/Runtime/Lighting/LightDefinition.cs.hlsl"
+#include "Packages/com.unity.render-pipelines.high-definition/Runtime/Sky/SkyUtils.hlsl"
+#include "Packages/com.unity.render-pipelines.high-definition/Runtime/Lighting/LightLoop/CookieSampling.hlsl"
 
-  TEXTURE2D(_SkyRTFullscreen);
-  TEXTURE2D(_SkyRTCubemap);
+/******************************************************************************/
+/******************************** END INCLUDES ********************************/
+/******************************************************************************/
 
-  /* Sampler for tables. */
-  #ifndef UNITY_SHADER_VARIABLES_INCLUDED
-      SAMPLER(s_linear_clamp_sampler);
-      SAMPLER(s_trilinear_clamp_sampler);
-  #endif
 
-  struct Attributes
+
+/******************************************************************************/
+/****************************** INPUT VARIABLES *******************************/
+/******************************************************************************/
+
+float4 _SkyParam; // x exposure, y multiplier, zw rotation (cosPhi and sinPhi)
+
+#define _Intensity          _SkyParam.x
+#define _CosPhi             _SkyParam.z
+#define _SinPhi             _SkyParam.w
+#define _CosSinPhi          _SkyParam.zw
+
+/* Render textures. */
+TEXTURE2D(_fullscreenSkyColorRT);
+TEXTURE2D(_fullscreenSkyTransmittanceRT);
+TEXTURE2D(_cubemapSkyColorRT);
+TEXTURE2D(_cubemapSkyTransmittanceRT);
+TEXTURE2D(_lastFullscreenCloudColorRT);
+TEXTURE2D(_lastFullscreenCloudTransmittanceRT);
+TEXTURE2D(_lastCubemapCloudColorRT);
+TEXTURE2D(_lastCubemapCloudTransmittanceRT);
+TEXTURE2D(_currFullscreenCloudColorRT);
+TEXTURE2D(_currFullscreenCloudTransmittanceRT);
+TEXTURE2D(_currCubemapCloudColorRT);
+TEXTURE2D(_currCubemapCloudTransmittanceRT);
+TEXTURE2D(_depthBuffer);
+
+/* Sampler for tables. */
+#ifndef UNITY_SHADER_VARIABLES_INCLUDED
+    SAMPLER(s_linear_clamp_sampler);
+    SAMPLER(s_trilinear_clamp_sampler);
+#endif
+
+/******************************************************************************/
+/**************************** END INPUT VARIABLES *****************************/
+/******************************************************************************/
+
+
+
+/******************************************************************************/
+/********************************* I/O TYPES **********************************/
+/******************************************************************************/
+
+struct Attributes
+{
+  uint vertexID : SV_VertexID;
+  UNITY_VERTEX_INPUT_INSTANCE_ID
+};
+
+struct Varyings
+{
+  float4 positionCS : SV_POSITION;
+  UNITY_VERTEX_OUTPUT_STEREO
+};
+
+struct SkyResult {
+  float4 color : SV_Target0;
+  float4 transmittance : SV_Target0;
+};
+
+struct CloudResult {
+  float4 color : SV_Target0;
+  float4 transmittance : SV_Target0;
+};
+
+/******************************************************************************/
+/******************************* END I/O TYPES ********************************/
+/******************************************************************************/
+
+
+
+/******************************************************************************/
+/******************************* VERTEX SHADER ********************************/
+/******************************************************************************/
+
+Varyings Vert(Attributes input)
+{
+    Varyings output;
+    UNITY_SETUP_INSTANCE_ID(input);
+    UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(output);
+    output.positionCS = GetFullScreenTriangleVertexPosition(input.vertexID, UNITY_RAW_FAR_CLIP_VALUE);
+    return output;
+}
+
+/******************************************************************************/
+/***************************** END VERTEX SHADER ******************************/
+/******************************************************************************/
+
+
+
+/******************************************************************************/
+/**************************** SKY FRAGMENT SHADER *****************************/
+/******************************************************************************/
+
+SkyResult RenderSky(Varyings input, bool cubemap) {
+  /* Final result. */
+  SkyResult r;
+  r.color = float4(0, 0, 0.5, 1);
+  r.transmittance = float4(0, 0.0, 0.5, 1);
+  return r;
+}
+
+SkyResult SkyCubemap(Varyings input) : SV_Target {
+  return RenderSky(input, true);
+}
+
+SkyResult SkyFullscreen(Varyings input) : SV_Target {
+  UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(input);
+  return RenderSky(input, false);
+}
+
+/******************************************************************************/
+/************************** END SKY FRAGMENT SHADER ***************************/
+/******************************************************************************/
+
+
+
+/******************************************************************************/
+/*************************** CLOUDS FRAGMENT SHADER ***************************/
+/******************************************************************************/
+
+SkyResult RenderClouds(Varyings input, bool cubemap) {
+  /* Final result. */
+  SkyResult r;
+  r.color = float4(0, 0.3, 0.0, 1);
+  r.transmittance = float4(0, 0.3, 0.0, 1);
+  return r;
+}
+
+SkyResult CloudsCubemap(Varyings input) : SV_Target {
+  return RenderSky(input, true);
+}
+
+SkyResult CloudsFullscreen(Varyings input) : SV_Target {
+  UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(input);
+  return RenderSky(input, false);
+}
+
+/******************************************************************************/
+/************************* END CLOUDS FRAGMENT SHADER *************************/
+/******************************************************************************/
+
+
+
+/******************************************************************************/
+/************************ COMPOSITING FRAGMENT SHADER *************************/
+/******************************************************************************/
+
+float4 Composite(Varyings input, bool cubemap, float exposure) {
+  float4 skyColor = float4(0.5, 0.0, 0.0, 1.0);
+  float2 textureCoordinate = float2(0.21,0.21);
+  skyColor += SAMPLE_TEXTURE2D_LOD(_fullscreenSkyColorRT,
+    s_linear_clamp_sampler, textureCoordinate, 0);
+  skyColor += SAMPLE_TEXTURE2D_LOD(_currFullscreenCloudColorRT,
+    s_linear_clamp_sampler, textureCoordinate, 0);
+  return skyColor;
+}
+
+float4 CompositeCubemap(Varyings input) : SV_Target {
+  UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(input);
+  return Composite(input, true, 1.0);
+}
+
+float4 CompositeFullscreen(Varyings input) : SV_Target {
+  return Composite(input, false, GetCurrentExposureMultiplier());
+}
+
+/******************************************************************************/
+/********************** END COMPOSITING FRAGMENT SHADER ***********************/
+/******************************************************************************/
+
+
+ENDHLSL
+
+SubShader
+{
+  /* Sky cubemap */
+  Pass
   {
-      uint vertexID : SV_VertexID;
-      UNITY_VERTEX_INPUT_INSTANCE_ID
-  };
+    ZWrite Off
+    ZTest Always
+    Blend Off
+    Cull Off
 
-  struct Varyings
+    HLSLPROGRAM
+        #pragma fragment SkyCubemap
+    ENDHLSL
+  }
+
+  /* Sky fullscreen */
+  Pass
   {
-      float4 positionCS : SV_POSITION;
-      UNITY_VERTEX_OUTPUT_STEREO
-  };
+    ZWrite Off
+    ZTest LEqual /* TODO: maybe turn off but turn on alpha blending for transmission? */
+    Blend Off
+    Cull Off
 
-  Varyings Vert(Attributes input)
+    HLSLPROGRAM
+        #pragma fragment SkyFullscreen
+    ENDHLSL
+  }
+
+  /* Clouds cubemap */
+  Pass
   {
-      Varyings output;
-      UNITY_SETUP_INSTANCE_ID(input);
-      UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(output);
-      output.positionCS = GetFullScreenTriangleVertexPosition(input.vertexID, UNITY_RAW_FAR_CLIP_VALUE);
-      return output;
+    ZWrite Off
+    ZTest Always
+    Blend Off
+    Cull Off
+
+    HLSLPROGRAM
+        #pragma fragment CloudsCubemap
+    ENDHLSL
   }
 
-  float3 RotationUp(float3 p, float2 cos_sin)
+  /* Clouds fullscreen */
+  Pass
   {
-      float3 rotDirX = float3(cos_sin.x, 0, -cos_sin.y);
-      float3 rotDirY = float3(cos_sin.y, 0,  cos_sin.x);
+    ZWrite Off
+    ZTest LEqual /* TODO: maybe turn off but turn on alpha blending for transmission? */
+    Blend Off
+    Cull Off
 
-      return float3(dot(rotDirX, p), p.y, dot(rotDirY, p));
+    HLSLPROGRAM
+        #pragma fragment CloudsFullscreen
+    ENDHLSL
   }
 
-  float4 GetColorWithRotation(float3 dir, float2 cos_sin)
+  /* Cubemap compositing. */
+  Pass
   {
-      return float4(0.0, 0.0, 0.5, 1.0);
+    ZWrite Off
+    ZTest Always
+    Blend Off
+    Cull Off
+
+    HLSLPROGRAM
+        #pragma fragment CompositeCubemap
+    ENDHLSL
   }
 
-  float4 RenderSky(Varyings input)
+  /* Fullscreen compositing. */
+  Pass
   {
-    float3 viewDirWS = GetSkyViewDirWS(input.positionCS.xy);
+    ZWrite Off
+    ZTest LEqual
+    Blend Off
+    Cull Off
 
-    // Reverse it to point into the scene
-    float3 dir = -viewDirWS;
-
-    return GetColorWithRotation(dir, _CosSinPhi);
+    HLSLPROGRAM
+        #pragma fragment CompositeFullscreen
+    ENDHLSL
   }
-
-  float4 Composite(Varyings input, bool cubemap, float exposure) {
-    float4 skyColor = float4(0.5, 0.0, 0.0, 1.0);
-    // float4 screenSpace = computeScreenPos(input.positionCS);
-    // float2 uv = input.xy;
-    /* TODO: branch will be slow? */
-    float2 textureCoordinate = float2(0.21,0.21);//input.positionCS.xy / input.positionCS.w;
-    // if (cubemap) {
-      // float4 skyColor = _SkyRTCubemap[computeScreenPos(input.xy)];
-    // } else {
-      skyColor += SAMPLE_TEXTURE2D_LOD(_SkyRTFullscreen, s_linear_clamp_sampler, textureCoordinate, 0);
-    // }
-    return skyColor;
-  }
-
-  float4 SkyCubemap(Varyings input) : SV_Target {
-    return RenderSky(input);
-  }
-
-  float4 SkyFullscreen(Varyings input) : SV_Target {
-    UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(input);
-    return RenderSky(input);
-  }
-
-  float4 CompositeCubemap(Varyings input) : SV_Target {
-    UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(input);
-    return Composite(input, true, 1.0);
-    // return RenderSky(input);
-  }
-
-  float4 CompositeFullscreen(Varyings input) : SV_Target {
-    return Composite(input, false, GetCurrentExposureMultiplier());
-    // return RenderSky(input);
-  }
-
-  ENDHLSL
-
-  SubShader
-  {
-    /* Sky cubemap */
-    Pass
-    {
-      ZWrite Off
-      ZTest Always
-      Blend Off
-      Cull Off
-
-      HLSLPROGRAM
-          #pragma fragment SkyCubemap
-      ENDHLSL
-    }
-
-    /* Sky fullscreen */
-    Pass
-    {
-      ZWrite Off
-      ZTest LEqual
-      Blend Off
-      Cull Off
-
-      HLSLPROGRAM
-          #pragma fragment SkyFullscreen
-      ENDHLSL
-    }
-
-    /* Cubemap compositing. */
-    Pass
-    {
-      ZWrite Off
-      ZTest Always
-      Blend Off
-      Cull Off
-
-      HLSLPROGRAM
-          #pragma fragment CompositeCubemap
-      ENDHLSL
-    }
-
-    /* Fullscreen compositing. */
-    Pass
-    {
-      ZWrite Off
-      ZTest LEqual
-      Blend Off
-      Cull Off
-
-      HLSLPROGRAM
-          #pragma fragment CompositeFullscreen
-      ENDHLSL
-    }
-  }
-  Fallback Off
+}
+Fallback Off
 }
