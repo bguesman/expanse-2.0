df16f2b09901c77971b947e9e028627771c5754f
Brad Guesman <james_guesman@alumni.brown.edu>
2020-10-16 21:32:26 -0700

    Cleaned up EnumParameter.cs

--- code/sky/EnumParameter.cs
+++ code/sky/EnumParameter.cs
@@ -1,14 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Diagnostics;
-using System.Linq;
-using System.Reflection;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEditor.Rendering;
 using UnityEditor;
-using Expanse;
 
 namespace Expanse {
   /* Generic enum parameter class. */
