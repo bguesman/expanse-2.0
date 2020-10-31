36a38e615ff2013478749d0c79a650147b9c1432
Brad Guesman <james_guesman@alumni.brown.edu>
2020-10-31 02:34:12 -0700

    Cleaned up mapping:

--- code/sky/ExpanseSkyMapping.hlsl
+++ code/sky/ExpanseSkyMapping.hlsl
@@ -3,13 +3,21 @@
 
 #include "../common/shaders/ExpanseSkyCommon.hlsl"
 
+/******************************************************************************/
+/****************************** PARAMETER MAPS ********************************/
+/******************************************************************************/
+
+/* This section includes parameter mapping functions that are used to
+ * implement the more holistic mapping functions for each table in
+ * the next section. */
+
 float fromUnitToSubUVs(float u, float resolution) {
-  float j = 0.5;
+  const float j = 0.5;
   return (j + u * (resolution + 1 - 2 * j)) / (resolution + 1);
 }
 
 float fromSubUVsToUnit(float u, float resolution) {
-  float j = 0.5;
+  const float j = 0.5;
   return (u * (resolution + 1) - j) / (resolution + 1 - 2 * j);
 }
 
@@ -25,8 +33,6 @@ float map_r(float r, float atmosphereRadius, float planetRadius) {
   return safeSqrt((r - planetRadius) / (atmosphereRadius - planetRadius));
 }
 
-/* Unmaps r, the distance from the planet origin, from its mapped uv
- * coordinate. Uses mapping from bruneton and neyer. */
 float unmap_r(float u_r, float atmosphereRadius, float planetRadius) {
   return planetRadius + ((u_r * u_r) * (atmosphereRadius - planetRadius));
 }
@@ -40,27 +46,17 @@ float2 map_r_mu_transmittance(float r, float mu, float atmosphereRadius, float p
   if (groundHit) {
     mu = clamp(mu, -1, cos_h);
     u_mu = 0.5 * pow(abs((cos_h - mu) / (1 + cos_h)), 0.2);
-    // KEEP FOR GROUND.
     if (floatGT(u_mu, 0.5-muStep/2)) {
       u_mu = 0.5 - muStep/2;
     }
-    // if (floatLT(u_mu, muStep/2)) {
-    //   u_mu = muStep/2;
-    // }
   } else {
     mu = clamp(mu, cos_h, 1);
     u_mu = 0.5 * pow(abs((mu - cos_h) / (1 - cos_h)), 0.2) + 0.5;
-    // KEEP FOR SKY.
     if (floatLT(u_mu, 0.5 + muStep/2)) {
       u_mu = 0.5 + muStep/2;
     }
-    // if (floatGT(u_mu, 1 - muStep/2)) {
-    //   u_mu = 1 - muStep/2;
-    // }
   }
-
   float u_r = safeSqrt((r - planetRadius) / (atmosphereRadius - planetRadius));
-
   return float2(u_r, fromUnitToSubUVs(u_mu, resMu));
 }
 
@@ -68,7 +64,6 @@ float2 unmap_r_mu_transmittance(float u_r, float u_mu, float atmosphereRadius,
   float planetRadius, float resMu) {
   u_mu = fromSubUVsToUnit(u_mu, resMu);
   float r = planetRadius + ((u_r * u_r) * (atmosphereRadius - planetRadius));
-
   float mu = 0.0;
   float h = r - planetRadius;
   float cos_h = -safeSqrt(h * (2 * planetRadius + h)) / (planetRadius + h);
@@ -77,134 +72,6 @@ float2 unmap_r_mu_transmittance(float u_r, float u_mu, float atmosphereRadius,
   } else {
     mu = clampCosine(pow(2 * (u_mu - 0.5), 5) * (1 - cos_h) + cos_h);
   }
-
-  return float2(r, mu);
-}
-
-/* Maps mu, the cosine of the viewing angle, into range 0-1. Uses
- * mapping from bruneton and neyer. */
-float map_mu(float r, float mu, float atmosphereRadius, float planetRadius,
-  float d, bool groundHit, float resMu) {
-
-  float u_mu = 0.0;
-  float muStep = 1/resMu;
-  float h = r - planetRadius;
-  float cos_h = -safeSqrt(h * (2 * planetRadius + h)) / (planetRadius + h);
-  if (groundHit) {
-    mu = min(mu, cos_h);
-    u_mu = 0.5 * pow((cos_h - mu) / (1 + cos_h), 0.5);
-    // KEEP FOR GROUND.
-    if (floatGT(u_mu, 0.5-muStep/2)) {
-      u_mu = 0.5 - muStep/2;
-    }
-    // if (floatLT(u_mu, muStep/2)) {
-    //   u_mu = muStep/2;
-    // }
-  } else {
-    mu = max(mu, cos_h);
-    u_mu = 0.5 * pow((mu - cos_h) / (1 - cos_h), 0.5) + 0.5;
-    // KEEP FOR SKY.
-    if (floatLT(u_mu, 0.5 + muStep/2)) {
-      u_mu = 0.5 + muStep/2;
-    }
-    // if (floatGT(u_mu, 1 - muStep/2)) {
-    //   u_mu = 1 - muStep/2;
-    // }
-  }
-
-  return fromUnitToSubUVs(u_mu, resMu);
-}
-
-/* Unmaps mu, the cosine of the viewing angle, from its mapped uv
- * coordinate. Uses mapping from bruneton and neyer.  */
-float unmap_mu(float u_r, float u_mu, float atmosphereRadius,
-  float planetRadius, float resMu) {
-  u_mu = fromSubUVsToUnit(u_mu, resMu);
-
-  float r = planetRadius + ((u_r * u_r) * (atmosphereRadius - planetRadius));
-
-  float mu = 0.0;
-  float h = r - planetRadius;
-  float cos_h = -safeSqrt(h * (2 * planetRadius + h)) / (planetRadius + h);
-  if (floatLT(u_mu, 0.5)) {
-    mu = clampCosine(cos_h - pow(u_mu * 2, 2) * (1 + cos_h));
-  } else {
-    mu = clampCosine(pow(2 * (u_mu - 0.5), 2) * (1 - cos_h) + cos_h);
-  }
-
-  return mu;
-}
-
-/* Unmaps mu, the cosine of the viewing angle, from its mapped uv
- * coordinate. Uses mapping from bruneton and neyer.  */
-float unmap_mu_with_r(float r, float u_mu, float atmosphereRadius,
-  float planetRadius, float resMu) {
-  u_mu = fromSubUVsToUnit(u_mu, resMu);
-
-  float mu = 0.0;
-  float h = r - planetRadius;
-  float cos_h = -safeSqrt(h * (2 * planetRadius + h)) / (planetRadius + h);
-  if (floatLT(u_mu, 0.5)) {
-    mu = clampCosine(cos_h - pow(u_mu * 2, 2) * (1 + cos_h));
-  } else {
-    mu = clampCosine(pow(2 * (u_mu - 0.5), 2) * (1 - cos_h) + cos_h);
-  }
-
-  return mu;
-}
-
-/* Maps r and mu together---slightly more efficient than mapping them
- * individually, since they share calculations. */
-float2 map_r_mu(float r, float mu, float atmosphereRadius, float planetRadius,
-  float d, bool groundHit, float resMu) {
-
-  float u_mu = 0.0;
-  float muStep = 1/resMu;
-  float h = r - planetRadius;
-  float cos_h = -safeSqrt(h * (2 * planetRadius + h)) / (planetRadius + h);
-  if (groundHit) {
-    mu = clamp(mu, -1, cos_h);
-    u_mu = 0.5 * pow((cos_h - mu) / (1 + cos_h), 0.5);
-    // KEEP FOR GROUND.
-    if (floatGT(u_mu, 0.5-muStep/2)) {
-      u_mu = 0.5 - muStep/2;
-    }
-    // if (floatLT(u_mu, muStep/2)) {
-    //   u_mu = muStep/2;
-    // }
-  } else {
-    mu = clamp(mu, cos_h, 1);
-    u_mu = 0.5 * pow((mu - cos_h) / (1 - cos_h), 0.5) + 0.5;
-    // KEEP FOR SKY.
-    if (floatLT(u_mu, 0.5 + muStep/2)) {
-      u_mu = 0.5 + muStep/2;
-    }
-    // if (floatGT(u_mu, 1 - muStep/2)) {
-    //   u_mu = 1 - muStep/2;
-    // }
-  }
-
-  float u_r = safeSqrt((r - planetRadius) / (atmosphereRadius - planetRadius));
-
-  return float2(u_r, fromUnitToSubUVs(u_mu, resMu));
-}
-
-/* Unmaps mu and r together---slightly more efficient than unmapping them
- * individually, since they share calculations.  */
-float2 unmap_r_mu(float u_r, float u_mu, float atmosphereRadius,
-  float planetRadius, float resMu) {
-  u_mu = fromSubUVsToUnit(u_mu, resMu);
-  float r = planetRadius + ((u_r * u_r) * (atmosphereRadius - planetRadius));
-
-  float mu = 0.0;
-  float h = r - planetRadius;
-  float cos_h = -safeSqrt(h * (2 * planetRadius + h)) / (planetRadius + h);
-  if (floatLT(u_mu, 0.5)) {
-    mu = clampCosine(cos_h - pow(u_mu * 2, 2) * (1 + cos_h));
-  } else {
-    mu = clampCosine(pow(2 * (u_mu - 0.5), 2) * (1 - cos_h) + cos_h);
-  }
-
   return float2(r, mu);
 }
 
@@ -216,20 +83,65 @@ float unmap_mu_naive(float u_mu, float resMu) {
   return (fromSubUVsToUnit(u_mu, resMu) * 2) - 1;
 }
 
-/* Maps mu_l, the cosine of the sun zenith angle, into range 0-1. Uses
- * mapping from bruneton and neyer. */
+float map_mu(float r, float mu, float atmosphereRadius, float planetRadius,
+  float d, bool groundHit, float resMu) {
+  float u_mu = 0.0;
+  float muStep = 1/resMu;
+  float h = r - planetRadius;
+  float cos_h = -safeSqrt(h * (2 * planetRadius + h)) / (planetRadius + h);
+  if (groundHit) {
+    mu = min(mu, cos_h);
+    u_mu = 0.5 * pow((cos_h - mu) / (1 + cos_h), 0.5);
+    if (floatGT(u_mu, 0.5-muStep/2)) {
+      u_mu = 0.5 - muStep/2;
+    }
+  } else {
+    mu = max(mu, cos_h);
+    u_mu = 0.5 * pow((mu - cos_h) / (1 - cos_h), 0.5) + 0.5;
+    if (floatLT(u_mu, 0.5 + muStep/2)) {
+      u_mu = 0.5 + muStep/2;
+    }
+  }
+  return fromUnitToSubUVs(u_mu, resMu);
+}
+
+float unmap_mu(float u_r, float u_mu, float atmosphereRadius,
+  float planetRadius, float resMu) {
+  u_mu = fromSubUVsToUnit(u_mu, resMu);
+  float r = planetRadius + ((u_r * u_r) * (atmosphereRadius - planetRadius));
+  float mu = 0.0;
+  float h = r - planetRadius;
+  float cos_h = -safeSqrt(h * (2 * planetRadius + h)) / (planetRadius + h);
+  if (floatLT(u_mu, 0.5)) {
+    mu = clampCosine(cos_h - pow(u_mu * 2, 2) * (1 + cos_h));
+  } else {
+    mu = clampCosine(pow(2 * (u_mu - 0.5), 2) * (1 - cos_h) + cos_h);
+  }
+  return mu;
+}
+
+float unmap_mu_with_r(float r, float u_mu, float atmosphereRadius,
+  float planetRadius, float resMu) {
+  u_mu = fromSubUVsToUnit(u_mu, resMu);
+  float mu = 0.0;
+  float h = r - planetRadius;
+  float cos_h = -safeSqrt(h * (2 * planetRadius + h)) / (planetRadius + h);
+  if (floatLT(u_mu, 0.5)) {
+    mu = clampCosine(cos_h - pow(u_mu * 2, 2) * (1 + cos_h));
+  } else {
+    mu = clampCosine(pow(2 * (u_mu - 0.5), 2) * (1 - cos_h) + cos_h);
+  }
+  return mu;
+}
+
 float map_mu_l(float mu_l) {
   return saturate((1.0 - exp(-3 * mu_l - 0.6)) / (1 - exp(-3.6)));
 }
 
-/* Unmaps mu_l, the cosine of the sun zenith angle, from its mapped uv
- * coordinate. Uses mapping from bruneton and neyer. */
 float unmap_mu_l(float u_mu_l) {
   return clampCosine((log(1.0 - (u_mu_l * (1 - exp(-3.6)))) + 0.6) / -3.0);
 }
 
-/* Maps nu, the cosine of the sun azimuth angle, into range 0-1. Uses
- * mapping from bruneton and neyer. */
 float map_nu(float nu) {
   float gamma = acos(nu) / PI;
   if (gamma >= 0.5) {
@@ -239,8 +151,6 @@ float map_nu(float nu) {
   }
 }
 
-/* Unmaps nu, the cosine of the sun azimuth angle, from its mapped uv
- * coordinate. Uses mapping from bruneton and neyer. */
 float unmap_nu(float u_nu) {
   if (u_nu > 0.5) {
     float gamma = (pow(((u_nu - 0.5) / 0.5), 2/3) / 2) + 0.5;
@@ -259,32 +169,15 @@ float unmap_theta(float u_theta, float resTheta) {
   return fromSubUVsToUnit(u_theta, resTheta) * 2 * PI;
 }
 
+/******************************************************************************/
+/**************************** END PARAMETER MAPS ******************************/
+/******************************************************************************/
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/******************************************************************************/
+/******************************** TABLE MAPS **********************************/
+/******************************************************************************/
 
 /* Returns u_mu_l, v. V is always zero, since the texture is effectively 1D. */
 float2 mapSky1DCoord(float mu_l) {
@@ -300,17 +193,14 @@ float2 unmapSky1DCoord(float u_mu_l, float _planetRadius) {
 float2 mapSky2DCoord(float r, float mu, float atmosphereRadius,
   float planetRadius, float d, bool groundHit, float resMu) {
   return map_r_mu_transmittance(r, mu, atmosphereRadius, planetRadius, d, groundHit, resMu);
-  // return float2(map_r_naive(r, atmosphereRadius, planetRadius), map_mu_naive(mu, resMu));
 }
 
 /* Returns r, mu. */
 float2 unmapSky2DCoord(float u_r, float u_mu,
   float atmosphereRadius, float planetRadius, float resMu) {
   return unmap_r_mu_transmittance(u_r, u_mu, atmosphereRadius, planetRadius, resMu);
-  // return float2(unmap_r_naive(u_r, atmosphereRadius, planetRadius), unmap_mu_naive(u_mu, resMu));
 }
 
-/* This parameterization was taken from Hillaire's 2020 model. */
 /* Returns u_r, u_mu_l. */
 float2 mapMSCoordinate(float r, float mu_l,
   float atmosphereRadius, float planetRadius) {
@@ -338,9 +228,9 @@ float2 unmapSkyRenderCoordinate(float r, float u_mu, float u_theta,
     unmap_theta(u_theta, resTheta));
 }
 
-/* Returns d from mu and theta angles. */
+/* Returns d from mu and theta angles and view point. */
 float3 mu_theta_to_d(float cos_mu, float theta, float3 O) {
-  /* Construct local frame. TODO: function? */
+  /* Construct local frame. */
   float3 y = normalize(O);
   float3 k = float3(1, 0, 0);
   float3 z = cross(y, k);
@@ -354,6 +244,7 @@ float3 mu_theta_to_d(float cos_mu, float theta, float3 O) {
   return normalize(dx + dy + dz);
 }
 
+/* Returns theta from view direction and view point. */
 float d_to_theta(float3 d, float3 O) {
   /* Construct local frame. */
   float3 y = normalize(O);
@@ -394,5 +285,9 @@ float4 unmapFrustumCoordinate(float3 uvw) {
 float3 mapFrustumCoordinate(float2 positionCS, float linearDepth) {
   return float3(positionCS/_currentScreenSize.xy, linearDepth / _farClip);
 }
+
+/******************************************************************************/
+/****************************** END TABLE MAPS ********************************/
+/******************************************************************************/
 
 #endif // EXPANSE_SKY_MAPPING_INCLUDED
